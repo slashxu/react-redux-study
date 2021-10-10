a2a63bb7b20f457cf3b25e809a5a4df2bec0800b
slashxu <sergiojsjr@tahoo.com.br>
2021-10-10 12:52:45 -0300

    Routes history

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,6 +1,7 @@
 import { select, call, put, all, takeLatest } from 'redux-saga/effects';
 import { addReserveSuccess, updateAmountSuccess } from './actions';
 import api from '../../../services/api';
+import history from '../../../services/history';
 
 function* addToReserve({ id }){
   const tripExists = yield select(
@@ -32,7 +33,9 @@ function* addToReserve({ id }){
       amount: 1,
     };
 
-    yield put(addReserveSuccess(data))
+    yield put(addReserveSuccess(data));
+
+    history.push('/reservas');
   }
 
 
