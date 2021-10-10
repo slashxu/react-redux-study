ba6f6b99caf89cd08cb58567f946068627a4a12f
slashxu <sergiojsjr@tahoo.com.br>
2021-10-10 12:27:37 -0300

    Consulting stock

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,5 +1,5 @@
 import { select, call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess, updateAmountReserve } from './actions';
+import { addReserveSuccess, updateAmountSuccess } from './actions';
 import api from '../../../services/api';
 
 function* addToReserve({ id }){
@@ -7,10 +7,22 @@ function* addToReserve({ id }){
     state => state.reserve.find(trip => trip.id === id)
   );
 
-  if(tripExists){ 
-    const amount = tripExists.amount + 1;
+  const myStock = yield call(api.get, `/stock/${id}`);
 
-    yield put(updateAmountReserve(id, amount));
+  const stockAmount = myStock.data.amount;
+  
+  const currentStock = tripExists ? tripExists.amount : 0; 
+
+  const amount = currentStock + 1;
+
+  if(amount > stockAmount){
+    alert('Quantidade maxima atingida.');
+    return;
+  }
+
+  if(tripExists){ 
+
+    yield put(updateAmountSuccess(id, amount));
 
   }else{
     const response = yield call(api.get, `trips/${id}` );
@@ -27,6 +39,24 @@ function* addToReserve({ id }){
 
 }
 
+
+function* updateAmount({ id, amount }){
+  if(amount <= 0) return;
+
+  const myStock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = myStock.data.amount;
+
+  if(amount > stockAmount){
+    alert('Quantidade maxima atingida.');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+
+}
+
 export default all([
-  takeLatest('ADD_RESERVE_REQUEST', addToReserve)
+  takeLatest('ADD_RESERVE_REQUEST', addToReserve),
+  takeLatest('UPDATE_RESERVE_REQUEST', updateAmount),
 ])
